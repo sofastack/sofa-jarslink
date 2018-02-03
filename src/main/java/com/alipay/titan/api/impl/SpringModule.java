a5d6e4b53b288279204c21eb9ec753f78978e04f
清英 <tengfei.fangtf@alibaba-inc.com>
2018-02-04 00:46:13 +0800

    add copyright

--- src/main/java/com/alipay/titan/api/impl/SpringModule.java
+++ src/main/java/com/alipay/titan/api/impl/SpringModule.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2018 Alibaba Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.titan.api.impl;
 
 import com.alipay.titan.api.Action;
@@ -68,9 +83,19 @@ public class SpringModule implements Module {
                 });
     }
 
+    /**
+     * 扫描模块里的ACTION
+     *
+     * @param applicationContext
+     * @param type
+     * @param keyFunction
+     * @param <T>
+     * @return
+     */
     private <T> Map<String, T> scanActions(ApplicationContext applicationContext, Class<T> type,
                                            Function<T, String> keyFunction) {
         Map<String, T> actions = Maps.newHashMap();
+        //find Action in module
         for (T action : applicationContext.getBeansOfType(type).values()) {
             String actionName = keyFunction.apply(action);
             if (isBlank(actionName)) {
