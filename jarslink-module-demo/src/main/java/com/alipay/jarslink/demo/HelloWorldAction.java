3941603c6e4574d09228b5753a5100df2ebe29d2
joe <1213812243@qq.com>
2018-03-29 17:36:30 +0800

    还原demo

--- jarslink-module-demo/src/main/java/com/alipay/jarslink/demo/HelloWorldAction.java
+++ jarslink-module-demo/src/main/java/com/alipay/jarslink/demo/HelloWorldAction.java
@@ -7,23 +7,20 @@ import com.alipay.jarslink.api.ModuleConfig;
  * 一个简单的Action实现
  */
 public class HelloWorldAction implements Action<ModuleConfig, ModuleConfig> {
-    private AnnotationAction annotationAction;
-
     @Override
     public ModuleConfig execute(ModuleConfig actionRequest) {
-        return annotationAction.execute(actionRequest);
+        ModuleConfig moduleConfig = new ModuleConfig();
+        moduleConfig.setName(actionRequest.getName());
+        moduleConfig.setEnabled(actionRequest.getEnabled());
+        moduleConfig.setVersion(actionRequest.getVersion());
+        moduleConfig.setModuleUrl(actionRequest.getModuleUrl());
+        moduleConfig.setProperties(actionRequest.getProperties());
+        moduleConfig.setOverridePackages(actionRequest.getOverridePackages());
+        return moduleConfig;
     }
 
     @Override
     public String getActionName() {
-        return annotationAction.getActionName();
-    }
-
-    public AnnotationAction getAnnotationAction() {
-        return annotationAction;
-    }
-
-    public void setAnnotationAction(AnnotationAction annotationAction) {
-        this.annotationAction = annotationAction;
+        return "helloworld";
     }
 }
