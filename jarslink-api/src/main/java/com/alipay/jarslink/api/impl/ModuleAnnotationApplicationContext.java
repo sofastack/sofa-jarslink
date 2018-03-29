c161363d49a0184d94e9578c8fcff1c242c7c8d3
清英 <tengfei.fangtf@alibaba-inc.com>
2018-03-29 22:54:56 +0800

    增加注释,优化代码

--- jarslink-api/src/main/java/com/alipay/jarslink/api/impl/ModuleAnnotationApplicationContext.java
+++ jarslink-api/src/main/java/com/alipay/jarslink/api/impl/ModuleAnnotationApplicationContext.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.util.Properties;
 
 /**
+ *  Spring Annotation Application Context
+ *
  * @author joe
  * @version 2018.03.28 23:06
  */
 public class ModuleAnnotationApplicationContext extends AnnotationConfigApplicationContext {
 
     public ModuleAnnotationApplicationContext(Properties properties) {
-        properties = properties == null ? new Properties() : properties;
-        ModuleUtil.registerModulePropertiesPlaceHolderConfigurer(this, properties);
+        Properties springProperties = (properties == null) ? new Properties() : properties;
+        ModuleUtil.registerModulePropertiesPlaceHolderConfigurer(this, springProperties);
     }
 }
--- jarslink-api/src/main/java/com/alipay/jarslink/api/impl/ModuleUtil.java
+++ jarslink-api/src/main/java/com/alipay/jarslink/api/impl/ModuleUtil.java
@@ -5,17 +5,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 
 import java.util.Properties;
 
 /**
+ *  模块工具类
+ *
  * @author joe
  * @version 2018.03.28 23:13
  */
 public class ModuleUtil {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ModuleUtil.class);
+
     public static final String MODULE_PROPERTY_PLACEHOLDER_CONFIGURER = "modulePropertyPlaceholderConfigurer";
 
     /**
@@ -24,8 +27,8 @@ public class ModuleUtil {
      * @param beanFactory
      */
     static BeanDefinitionRegistry registerModulePropertiesPlaceHolderConfigurer(BeanDefinitionRegistry
-                                                                                               beanFactory,
-                                                                                       Properties properties) {
+                                                                                        beanFactory,
+                                                                                Properties properties) {
         //通过GenericBeanDefinition创建bean
         GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
         //设置bean资源属性的配置器
