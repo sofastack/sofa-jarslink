af1ff428bccc540d40f27363ce31b4d80b168c28
JoeKerouac <1213812243@qq.com>
2018-04-02 22:56:46 +0800

    删除多余导包

--- jarslink-api/src/main/java/com/alipay/jarslink/api/impl/ModuleManagerImpl.java
+++ jarslink-api/src/main/java/com/alipay/jarslink/api/impl/ModuleManagerImpl.java
@@ -27,7 +27,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
