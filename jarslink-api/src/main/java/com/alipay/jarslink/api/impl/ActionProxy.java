04e3ce5f29998a204111600c0d0d894ad103a8a4
JoeKerouac <1213812243@qq.com>
2018-04-16 23:02:30 +0800

    更改模块卸载逻辑

---
+++ jarslink-api/src/main/java/com/alipay/jarslink/api/impl/ActionProxy.java
@@ -0,0 +1,43 @@
+package com.alipay.jarslink.api.impl;
+
+import com.alipay.jarslink.api.Action;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * action代理
+ *
+ * @author joe
+ * @version 2018.04.16 22:33
+ */
+public class ActionProxy<R, T> implements Action<R, T> {
+    private Action<R, T> action;
+
+    /**
+     * 默认构造器
+     *
+     * @param action 要代理的Action
+     */
+    public ActionProxy(Action<R, T> action) {
+        this.action = action;
+    }
+
+    /**
+     * 关闭action，便于回收
+     */
+    public void destroy() {
+        action = null;
+    }
+
+    @Override
+    public T execute(R actionRequest) {
+        checkNotNull(action, "current module was closed");
+        return action.execute(actionRequest);
+    }
+
+    @Override
+    public String getActionName() {
+        checkNotNull(action, "current module was closed");
+        return action.getActionName();
+    }
+}
